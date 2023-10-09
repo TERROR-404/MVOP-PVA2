b70dbc05860b9cc4c46341a4d840e4f6258f32ed
Bohumil Čmerda <69761253+TERROR-404@users.noreply.github.com>
2023-10-09 13:55:05 +0200

    Create string-uloha5.cs

---
+++ string-uloha5.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> d = new Dictionary<string, int>();
+            string[] input = File.ReadAllLines("Z:/rahul.txt");
+            string text = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] a = input[i].Split(" ");
+                foreach (var item in a)
+                {
+                    if (d.ContainsKey(item))
+                    {
+                        d[item]++;
+                    }
+                    else
+                    {
+                        d.Add(item,1);
+                    }
+
+                }
+            }
+            double totalValue = 0;
+            foreach (var item in d)
+            {
+                totalValue += item.Value;
+            }
+
+            foreach (var item in d)
+            {
+                Console.WriteLine($"{item.Key}, {item.Value}, {item.Value/totalValue}%");
+            }
+        }
+    }
+}
