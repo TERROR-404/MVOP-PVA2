a4c6f0d734e689a96071d3ddff77ae67b43aac77
Bohumil Čmerda <69761253+TERROR-404@users.noreply.github.com>
2023-10-11 12:12:10 +0200

    Update string-uloha4.cs oprava tabu

--- string-uloha4.cs
+++ string-uloha4.cs
@@ -10,35 +10,35 @@ namespace ConsoleApp1
         {
           string file = "rahul.txt";
             string text = File.ReadAllText(file);
-        		Dictionary<string,List<int>> d = new Dictionary<string,List<int>>();
-        		string[] t = text.Split('\n',' ');
-        		foreach(var i in t)
-        		{
-        			if(d.ContainsKey(i))
-        			{
-        				int a = text.IndexOf(i,d[i][d[i].Count-1]+1 , text.Length-(d[i][d[i].Count-1])-1);
-        				d[i].Add(a);
-        			}
-        			else
-        			{
-        				List<int> l = new List<int>();
-        				int a = text.IndexOf(i);
-        				l.Add(a);
-        				d.Add(i,l);
-        			}
-        		}
-          using(StreamWriter sw = new StreamWriter(file))
-          {
-            foreach(var i in d)
-        		{
-        			sw.Write(i.Key);
-        			foreach(var j in i.Value)
-        			{
-        				sw.Write(" {0},",j);
-        			}
-        			sw.WriteLine();
-        		}
-          }
+            Dictionary<string,List<int>> d = new Dictionary<string,List<int>>();
+            string[] t = text.Split('\n',' ');
+            foreach(var i in t)
+            {
+                if(d.ContainsKey(i))
+                {
+                    int a = text.IndexOf(i,d[i][d[i].Count-1]+1 , text.Length-(d[i][d[i].Count-1])-1);
+                    d[i].Add(a);
+                }
+                else
+                {
+                    List<int> l = new List<int>();
+                    int a = text.IndexOf(i);
+                    l.Add(a);
+                    d.Add(i,l);
+                }
+            }
+            using(StreamWriter sw = new StreamWriter(file))
+            {
+                foreach(var i in d)
+            	{
+                    sw.Write(i.Key);
+                    foreach(var j in i.Value)
+                    {
+                        sw.Write(" {0},",j);
+                    }
+                    sw.WriteLine();
+        	    }
+            }
         }
     }
 }
