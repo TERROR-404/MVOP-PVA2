eaf481ce94ff11184e6c6f29fb25e3841b6a33f6
Bohumil Čmerda <69761253+TERROR-404@users.noreply.github.com>
2023-10-29 18:46:16 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input1 = "jan.novak@seznam.cz";
+            string input2 = "jannovak@mail.cz";
+            string input3 = "jan_novak111@centrum.cz";
+            string input4 = "Pepa.@Prase..";
+            Console.WriteLine(IsMail(input1));
+            Console.WriteLine(IsMail(input2));
+            Console.WriteLine(IsMail(input3));
+            Console.WriteLine(IsMail(input4));
+        }
+        public static bool IsMail(string s)
+        {
+            Regex r = new Regex(@"^\w+(\.\w+)*@(\w+\.)+\w{2,4}$");
+            if (r.IsMatch(s))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
