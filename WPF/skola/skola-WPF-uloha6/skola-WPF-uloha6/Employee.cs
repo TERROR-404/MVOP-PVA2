d33ad68080eeb9e3a3393df9bfe7d1a6a6db18dd
Bohumil Čmerda <69761253+TERROR-404@users.noreply.github.com>
2024-03-11 15:21:39 +0100

    uloha 6 nedodelana

---
+++ WPF/skola/skola-WPF-uloha6/skola-WPF-uloha6/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skola_WPF_uloha6
+{
+    internal class Employee : Person
+    {
+        public string Education { get; set; }
+        public string WorkPosition { get; set; }
+        public string GrossSalary { get; set; }
+        public Employee(string education, string workPosition, string grossSalary, string name, string surname, int yearOfBirth) : base(name, surname, yearOfBirth)
+        {
+            Education = education;
+            WorkPosition = workPosition;
+            GrossSalary = grossSalary;
+        }
+    }
+}
---
+++ WPF/skola/skola-WPF-uloha6/skola-WPF-uloha6/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skola_WPF_uloha6
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int YearOfBirth { get; set; }
+        public Person(string name, string surname, int yearOfBirth)
+        {
+            Name = name;
+            Surname = surname;
+            YearOfBirth = yearOfBirth;
+        }
+    }
+}
