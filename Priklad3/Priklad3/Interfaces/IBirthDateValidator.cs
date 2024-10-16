7bf0fe2c475e9d845791a99bcc2e098589211c55
TERROR-404 <69761253+TERROR-404@users.noreply.github.com>
2024-10-16 10:13:15 +0200

    dis

---
+++ Priklad3/Priklad3/Interfaces/IBirthDateValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priklad3.Interfaces
+{
+    interface IBirthDateValidator
+    {
+        bool isValid(string birthDate, out DateTime retVal);
+    }
+}
---
+++ Priklad3/Priklad3/Person.cs
@@ -0,0 +1,23 @@
+﻿using Priklad3.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priklad3
+{
+    internal class Person
+    {
+        readonly IBirthDateValidator birthDateValidator;
+        readonly INameValidator nameValidator;
+        readonly ISSNValidator ssnValidator;
+
+        public Person(IBirthDateValidator birthDateValidator, INameValidator nameValidator, ISSNValidator ssnValidator)
+        {
+            this.birthDateValidator = birthDateValidator;
+            this.nameValidator = nameValidator;
+            this.ssnValidator = ssnValidator;
+        }
+    }
+}
